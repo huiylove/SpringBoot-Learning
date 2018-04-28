be99a9f7f94e9377f5af43fa435eebdad1a2c343
huiylove <huiy6.2991@aliyun.com>
2018-04-28 10:56:57 +0800

    使用Spring Security进行安全控制

--- springboot-helloworld/src/main/java/com/huiy/springboothelloworld/web/controller/ThymeleafController.java
+++ springboot-helloworld/src/main/java/com/huiy/springboothelloworld/web/controller/ThymeleafController.java
@@ -15,12 +15,21 @@ import springfox.documentation.annotations.ApiIgnore;
 @Controller
 public class ThymeleafController {
 	
+	
+	
     @ApiIgnore
 	@RequestMapping("/")
 	public String index(ModelMap model){
-		model.addAttribute("host","https://github.com/huiylove/");
-        // return模板文件的名称，对应src/main/resources/templates/index.html
+    	//model.addAttribute("host","https://github.com/huiylove/");
+        //return模板文件的名称     对应src/main/resources/templates/index.html
 		return "index";
+	}
+    
+
+    @ApiIgnore
+	@RequestMapping("/loginSuccess")
+	public String login(){
+		return "loginSuccess";
 	}
 	
 
